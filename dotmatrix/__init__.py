a6bea47a9022f38ed87ac0d392d9977cc40831a6
Tim Fischer <me@timfi.dev>
2021-08-17 11:23:01 +0200

    Added proper displays submodule
    Dumping all possible displays into a single file just doesn't
    sit right with me.

--- dotmatrix/__init__.py
+++ dotmatrix/__init__.py
@@ -1,6 +1,6 @@
-from .display import *
+from .displays import *
 from .matrix import *
 
 __version__ = "0.2.0"
 
-__all__ = matrix.__all__ + display.__all__  # type: ignore
+__all__ = matrix.__all__ + displays.__all__  # type: ignore
